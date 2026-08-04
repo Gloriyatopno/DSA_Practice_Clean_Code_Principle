0fdca64d26639f6af36b6e2154206ff6e53b0a79
Gloriya Topno <gloriyatopno8@gmail.com>
2026-08-04 19:05:47 +0530

    Implement Stack

---
+++ Stack.py
@@ -0,0 +1,14 @@
+#Stack
+stack = []
+
+stack.append(60)
+stack.append(50)
+stack.append(40)
+
+print("Stack:", stack)
+
+print("Removed:", stack.pop())
+
+print("Top Element:", stack[-1])
+
+print("Stack after pop:", stack)
